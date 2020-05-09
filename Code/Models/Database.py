3625d5bfff2de7dac0d4408fb84ff3fefbbd4752
mdmjg <mdj308@nyu.edu>
2020-05-09 10:28:09 +0400

    updated database

--- Code/Models/Database.py
+++ Code/Models/Database.py
@@ -2,6 +2,7 @@ import pymysql.cursors
 from Models.CountryList import CountryList
 from Models.StatsScraper import StatsScraper
 from Models.TravelScraper import TravelScraper
+from Models.HealthcareScraper import HealthcareScraper
 
 
 class Database:
@@ -19,6 +20,7 @@ class Database:
         self.CountryList = CountryList()
         self.StatsScraper = StatsScraper()
         self.TravelScraper = TravelScraper()
+        self.Healthcare = HealthcareScraper()
 
     def searchCountry(self, country):
         print(country)
@@ -40,21 +42,27 @@ class Database:
 
     def addCountries(self):
         cursor = self.conn.cursor()
-        query = "INSERT INTO Country (countryName, numCases, numDeaths, numRecovered, numTests, numHospitalBeds, latestTravelRestriction) VALUES (%s, 0, 0, 0, 0, 0, NULL)"
+        query = "INSERT INTO Country (countryName, numCases, numDeaths, numRecovered, numDoctors, numHospitalBeds, latestTravelRestriction) VALUES (%s, 0, 0, 0, 0, 0, NULL)"
         for country in self.CountryList.countryList:
             cursor.execute(query, country)
             self.conn.commit()
         cursor.close()
         print("success")
 
-    def updateCountryHealthcare(
-        self, country, numCases, numDeaths, numRecovered, numTests, numHospitalBeds
-    ):
+    def updateCountryHealthcare(self, country, numDoctors, numHospitalBeds):
         cursor = self.conn.cursor()
-        query = "UPDATE Country SET numCases = %d, numDeaths = %d, numRecovered = %d, numTests = %d, numHospitalBeds = %d WHERE countryName = %s)"
+        query = "UPDATE Country SET numDoctors = %d, numHospitalBeds = %d WHERE countryName = %s)"
         cursor.execute(
-            query,
-            (numCases, numDeaths, numRecovered, numTests, numHospitalBeds, country),
+            query, (numDoctors, numHospitalBeds, country),
+        )
+        self.conn.commit()
+        cursor.close()
+
+    def updateCountryStats(self, country, numCases, numDeaths, numRecovered):
+        cursor = self.conn.cursor()
+        query = "UPDATE Country SET numCases = %d, numDeaths = %d, numRecovered = %d WHERE countryName = %s)"
+        cursor.execute(
+            query, (numCases, numDeaths, numRecovered, country),
         )
         self.conn.commit()
         cursor.close()
@@ -69,6 +77,14 @@ class Database:
         cursor.close()
 
     def updateCountryHospitalBeds(self, country):
+        health = self.Healthcare.scrapeHealthcare()
+        if health:
+            for index, row in health.iterrows():
+                countryName = row["countryName"]
+                numDoctors = row["numDoctors"]
+                numHospitalBeds = row["numHospitalBeds"]
+                self.updateCountryHealthcare(countryName, numDoctors, numHospitalBeds)
+
         pass
 
     def updateWHO(self):
@@ -79,21 +95,21 @@ class Database:
 
     def updateWorldStats(self):
         pass
-# <<<<<<< HEAD
+
+    # <<<<<<< HEAD
 
     def updateAllCoutriesHealthcare(self):
         # how do we  time this?
         allCases = self.StatsScraper.scrapeCases()
-        df = allCases[0]
-        for index, row in df.iterrows():
-            countryName = row["countryName"]
-            numCases = row["numCases"]
-            numDeaths = row["numDeaths"]
-            numRecovered = row["numRecovered"]
-            self.updateCountryHealthcare(
-                countryName, numCases, numDeaths, numRecovered, 0, 0
-            )
-        print("updated all countries")
+        if allCases:
+            df = allCases[0]
+            for index, row in df.iterrows():
+                countryName = row["countryName"]
+                numCases = row["numCases"]
+                numDeaths = row["numDeaths"]
+                numRecovered = row["numRecovered"]
+                self.updateCountryStats(countryName, numCases, numDeaths, numRecovered)
+            print("updated all countries")
 
     def updateAllCountriesTravel(self):
         travel = self.TravelScraper.scrapeTravel()
@@ -101,6 +117,7 @@ class Database:
             countryName = row["countryName"]
             latestTravelRestriction = row["travelAdv"]
             self.updateCountryTravelRestrictions(countryName, latestTravelRestriction)
+
 
 # =======
 # >>>>>>> 15caf204401816919fbd989800776cdc644c5c1a
--- Code/Models/TravelScraper.py
+++ Code/Models/TravelScraper.py
@@ -5,75 +5,111 @@ from datetime import date
 import requests
 import pandas as pd
 
+
 class TravelScraper:
-	lastScraped = date(2020, 4, 24)
+    lastScraped = date(2020, 4, 24)
 
-	def checkVersion(self): # Checks for last scraping time--scraping is only done if the current version is outdated
-		if self.lastScraped != date.today():
-			return True
-		else:
-			return False
+    def checkVersion(
+        self,
+    ):  # Checks for last scraping time--scraping is only done if the current version is outdated
+        if self.lastScraped != date.today():
+            return True
+        else:
+            return False
 
-	def getListBoundaries(self, soupWiki): # Finds the specific start and end of the country list within the article
-		listStart = 0
-		listEnd = 0
+    def getListBoundaries(
+        self, soupWiki
+    ):  # Finds the specific start and end of the country list within the article
+        listStart = 0
+        listEnd = 0
 
-		for i in range(90,110): # Some initial angling is done to speed up the search
-			if("flagicon" in str(soupWiki.find_all("li")[i].contents[0])): # Every country entry contains a flag icon
-				listStart = i
-				break
+        for i in range(90, 110):  # Some initial angling is done to speed up the search
+            if "flagicon" in str(
+                soupWiki.find_all("li")[i].contents[0]
+            ):  # Every country entry contains a flag icon
+                listStart = i
+                break
 
-		for j in range(listStart,250):
-			if("flagicon" not in str(soupWiki.find_all("li")[j].contents[0])):
-				listEnd = j
-				break
+        for j in range(listStart, 250):
+            if "flagicon" not in str(soupWiki.find_all("li")[j].contents[0]):
+                listEnd = j
+                break
 
-		return listStart, listEnd
+        return listStart, listEnd
 
-	def scrapeTravel(self):
+    def scrapeTravel(self):
 
-		if self.checkVersion() == False:
-			return None
+        if self.checkVersion() == False:
+            return None
 
-		travelWiki = requests.get("https://en.wikipedia.org/w/index.php?title=Travel_restrictions_related_to_the_COVID-19_pandemic&oldid=954913741")
-		if (travelWiki.status_code != 200):
-			return None
+        travelWiki = requests.get(
+            "https://en.wikipedia.org/w/index.php?title=Travel_restrictions_related_to_the_COVID-19_pandemic&oldid=954913741"
+        )
+        if travelWiki.status_code != 200:
+            return None
 
-		soupWiki = bs(travelWiki.text,"html.parser")
+        soupWiki = bs(travelWiki.text, "html.parser")
 
-		listStart, listEnd = self.getListBoundaries(soupWiki)
-		print(listStart, listEnd)
+        listStart, listEnd = self.getListBoundaries(soupWiki)
+        print(listStart, listEnd)
 
-		travelAdv = ""
-		countries = []
-		travelAdvs = []
+        travelAdv = ""
+        countries = []
+        travelAdvs = []
 
-		for countryNum in range(listStart, listEnd): # Country entries are between li tags of these numbers--manually checked
-			for i in range(0,len(soupWiki.find_all("li")[countryNum].contents)): # Each entry has multiple elements
-				if "<span" in str(soupWiki.find_all("li")[countryNum].contents[i]) or "<sup" in str(soupWiki.find_all("li")[countryNum].contents[i]): # If it's something beside a text (a reference or a redirect), ignore
-					continue
-				elif "<a" in str(soupWiki.find_all("li")[countryNum].contents[i]): # If it's a link, just get the text
-					travelAdv += str(soupWiki.find_all("li")[countryNum].contents[i].text)
-				else: # Else it's plain text and is good to add
-					travelAdv += str(soupWiki.find_all("li")[countryNum].contents[i])
+        for countryNum in range(
+            listStart, listEnd
+        ):  # Country entries are between li tags of these numbers--manually checked
+            for i in range(
+                0, len(soupWiki.find_all("li")[countryNum].contents)
+            ):  # Each entry has multiple elements
+                if "<span" in str(
+                    soupWiki.find_all("li")[countryNum].contents[i]
+                ) or "<sup" in str(
+                    soupWiki.find_all("li")[countryNum].contents[i]
+                ):  # If it's something beside a text (a reference or a redirect), ignore
+                    continue
+                elif "<a" in str(
+                    soupWiki.find_all("li")[countryNum].contents[i]
+                ):  # If it's a link, just get the text
+                    travelAdv += str(
+                        soupWiki.find_all("li")[countryNum].contents[i].text
+                    )
+                else:  # Else it's plain text and is good to add
+                    travelAdv += str(soupWiki.find_all("li")[countryNum].contents[i])
 
-			# Split the created clean advisory into country and info
-			countries.append(travelAdv.split(": ")[0])
-			travelAdvs.append(travelAdv.split(": ")[1])
+            # Split the created clean advisory into country and info
+            countries.append(travelAdv.split(": ")[0])
+            travelAdvs.append(travelAdv.split(": ")[1])
 
-			print(travelAdv, "\n")
-			travelAdv = ""
+            print(travelAdv, "\n")
+            travelAdv = ""
 
+        # Country name variations between this dataset and the main database, manually checked - will be used to harmonise them
+        missingTravel = [
+            "Taiwan",
+            "UAE",
+            "Democratic Republic of the Congo",
+            "The Gambia",
+            "Czech Republic",
+            "South Korea",
+            "United States",
+        ]
+        JHTravel = [
+            "Taiwan*",
+            "United Arab Emirates",
+            "Congo (Kinshasa)",
+            "Gambia",
+            "Czechia",
+            "Korea, South",
+            "US",
+        ]
 
-		# Country name variations between this dataset and the main database, manually checked - will be used to harmonise them
-		missingTravel = ["Taiwan", "UAE", "Democratic Republic of the Congo", "The Gambia", "Czech Republic", "South Korea", "United States"]
-		JHTravel = ["Taiwan*", "United Arab Emirates", "Congo (Kinshasa)", "Gambia", "Czechia", "Korea, South", "US"]
-		
-		# Create dataframe
-		dataTravel = {'countryName': countries, 'travelAdv': travelAdvs}
-		dfTravel = pd.DataFrame(dataTravel, columns=['countryName', 'travelAdv'])
-		dfTravel = dfTravel.replace(missingTravel, JHTravel)
+        # Create dataframe
+        dataTravel = {"countryName": countries, "travelAdv": travelAdvs}
+        dfTravel = pd.DataFrame(dataTravel, columns=["countryName", "travelAdv"])
+        dfTravel = dfTravel.replace(missingTravel, JHTravel)
 
-		self.lastScraped = date.today() # Update dataset version
+        self.lastScraped = date.today()  # Update dataset version
 
-		return dfTravel
+        return dfTravel
